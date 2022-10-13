dac9780341d3165a7eb08829c4147bd8f04b5f16
sambhu513 <sambhubaburaj513@gmail.com>
2022-10-13 13:22:18 +0530

    host

--- Helper-User/Home-helper.js
+++ Helper-User/Home-helper.js
@@ -96,13 +96,13 @@ else{
 if(!wishlist)
 {
 
-    res.render("user/Home", { title: "Express",name:user,user: req.session.user ,Product:ProductList,Category:listofcat ,length:length,banner:banner,wishlist:null,cartdata:await cartdata(req.session.user)});
+    res.render("User/Home", { title: "Express",name:user,user: req.session.user ,Product:ProductList,Category:listofcat ,length:length,banner:banner,wishlist:null,cartdata:await cartdata(req.session.user)});
  
 } 
 
 else{
 
-    res.render("user/Home", { name:user,title: "Express",user: req.session.user ,Product:ProductList,Category:listofcat ,length:length,banner:banner,wishlist:wishlist.Products,cartdata:await cartdata(req.session.user)});
+    res.render("User/Home", { name:user,title: "Express",user: req.session.user ,Product:ProductList,Category:listofcat ,length:length,banner:banner,wishlist:wishlist.Products,cartdata:await cartdata(req.session.user)});
 
 }
 
